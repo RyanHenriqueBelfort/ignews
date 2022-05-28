a7e87fac6321d1035dac04d295260871537083cd
Ryan Henrique de Assis Belfort <70604408+RyanHenriqueBelfort@users.noreply.github.com>
2022-05-28 12:31:26 -0300

    Integração com Stripe Api

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,6 +1,9 @@
 import styles from './styles.module.scss'
 
-export function SubscribeButton(){
+interface SubscribeButtonProps{
+  priceId: string;
+}
+export function SubscribeButton({ priceId }: SubscribeButtonProps){
   return(
     <button
       type="button"
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,23 @@
+import { GetStaticProps } from 'next'
 import Head from 'next/head'
 import { SubscribeButton } from '../components/SubscribeButton'
+import { stripe } from '../services/stipe'
 
 import styles from './home.module.scss'
 
-export default function Home() {
+// 3 formas principais de fazer uma chamada API
+//    Client-side: so depois que a pagina já foi carregada
+//    Server-side: assim que a pagina é exibido em tela
+//    Static Site Generation: 1 vez só ate da o tempo do revalidate
+
+interface HomeProps {
+  product: {
+    priceId: string
+    amount: number
+  }
+}
+export default function Home({product} : HomeProps) {
+
   return (
     <>
     <Head>
@@ -16,12 +30,60 @@ export default function Home() {
           <h1>News about the <span>React</span> word.</h1> 
           <p>
             Get access to all the publication <br/>
-            <span>for $9.90 month</span>
+              <span>for {product.amount} month</span>
           </p>
-          <SubscribeButton></SubscribeButton>
+          <SubscribeButton
+            priceId={product.priceId}
+          ></SubscribeButton>
         </section>
         <img src="/images/avatar.svg" alt="Girls coding" />
       </main>
     </>
   )
+}
+
+// USANDO SSR
+
+// export const getServerSideProps: GetServerSideProps =async () => {
+//   const price = await stripe.prices.retrieve('price_1L3rcVJckrmTFuRguiwEzgTI', ) 
+//   //pegando o produto
+
+//   const product = {
+//     priceId: price.id,
+//     amount: new Intl.NumberFormat('pt-BR', { 
+//       style: 'currency', 
+//       currency: 'BRL'
+//      }).format(price.unit_amount / 100)
+//   }  
+
+//   return {
+//     props: {
+//       product
+//     }
+//   }
+
+// }
+
+
+// USANDO SSR
+
+export const getStaticProps: GetStaticProps =async () => {
+  const price = await stripe.prices.retrieve('price_1L3rcVJckrmTFuRguiwEzgTI', ) 
+  //pegando o produto
+
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat('pt-BR', { 
+      style: 'currency', 
+      currency: 'BRL'
+     }).format(price.unit_amount / 100)
+  }  
+
+  return {
+    props: {
+      product
+    },
+    revalidate: 60 * 60 * 24 //24horas
+  }
+  
 }
