022bbc21e1b3dc1909df65de4371dbd7c2bf6911
Ryan Henrique de Assis Belfort <70604408+RyanHenriqueBelfort@users.noreply.github.com>
2022-06-04 00:07:05 -0300

    webhook stripe

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -5,7 +5,6 @@ import GithubProvider from "next-auth/providers/github"
 import { fauna } from '../../../services/fauna'
 
 export default NextAuth({
-  // Configure one or more authentication providers
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_ID,
